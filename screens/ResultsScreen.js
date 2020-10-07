5103ea83d1a319eb7bf84660a79c4c764fde6bae
MichaelMaryanoff <mmaryanoff@gmail.com>
2020-10-07 14:24:10 -0500

    Initial setup

---
+++ screens/ResultsScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const ResultsScreen = () => {
+  return (
+    <View>
+      <Text>ResultsScreen</Text>
+    </View>
+  );
+};
+
+export default ResultsScreen;
+
+const styles = StyleSheet.create({});
---
+++ screens/SpinScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const SpinScreen = () => {
+    return (
+        <View>
+            <Text>SpinScreen</Text>
+        </View>
+    )
+}
+
+export default SpinScreen
+
+const styles = StyleSheet.create({})
